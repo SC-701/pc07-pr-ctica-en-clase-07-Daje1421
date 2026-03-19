17dfcf243d19a40a8f0ed711f1e69e5a8825a400
daniel rivera <daje1421@gmail.com>
2026-03-18 20:24:04 -0600

    commit

---
+++ Producto.API/BD/dbo/Stored Procedures/ObtenerProductos.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE ObtenerProductos 
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT     Categorias.Id AS Expr1, Categorias.Nombre AS Expr2, Categorias.*, Producto.*, SubCategorias.*
+FROM            Categorias INNER JOIN
+                         Producto ON Categorias.Id = Producto.Id INNER JOIN
+                         SubCategorias ON Categorias.Id = SubCategorias.Id 
+END
---
+++ Producto.API/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producto.API
+{
+    public class Class1
+    {
+    }
+}
